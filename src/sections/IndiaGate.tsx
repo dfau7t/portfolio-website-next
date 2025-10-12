86b5dbd063d01522730883efac8bff0ef32d7d84
dfau7t <psrivatsarp@gmail.com>
2025-10-12 21:47:39 +0530

    updated india gate project

--- src/sections/IndiaGate.tsx
+++ src/sections/IndiaGate.tsx
@@ -6,22 +6,8 @@ export const IndiaGate = () => {
     return (
         <div>
             <Title name={"India Gate"}/>
-            <div className="mx-5 grid grid-cols-3 gap-2.5">
-              <div className="col-span-1 frame overflow-clip">
-                <Image 
-                  className="w-full h-full"
-                  src={ig} 
-                  alt="India Gate" />
-              </div>
-              <div className="col-span-2">
-                <div className="grid grid-rows-3 gap-2.5">
-                  <iframe 
-                    className="w-full h-full row-span-2 frame overflow-clip aspect-video"
-                    src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7292573457404375042?compact=1" 
-                    title="India Gate" 
-                    allowFullScreen
-                  />
-                  <div className="frame row-span-1 px-5 py-2.5 space-y-5">
+            <div className="mx-5 grid grid-cols-1 grid-rows-3 md:grid-rows-3 md:grid-cols-5 h-[95vh] gap-2.5">
+                <div className="frame col-span-1 row-span-1 md:col-span-5 md:row-span-1 px-5 py-2.5 space-y-5 overflow-scroll">
                     <p>
                       This project, undertaken during my internship at a media studio, was a large-scale public art installation 
                       for the Election Commission of India. The challenge was to create a powerful visual narrative promoting 
@@ -40,9 +26,20 @@ export const IndiaGate = () => {
                       It was a unique opportunity to use cutting-edge technology to create a moment of shared public experience, turning a civic responsibility 
                       into an unforgettable work of art.
                     </p>
-                  </div>
+                </div>                
+                <iframe 
+                  className="w-full h-full col-span-1 row-span-1 md:col-span-3 md:row-span-2 frame overflow-clip aspect-video"
+                  src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7292573457404375042?compact=1" 
+                  title="India Gate" 
+                  allowFullScreen
+                />                
+                <div className="col-span-1 row-span-1 md:col-span-2 md:row-span-2 frame overflow-clip content-center w-full">
+                  <Image 
+                    src={ig} 
+                    alt="India Gate" />
                 </div>
-              </div>
+
+
             </div>
         </div>
     );
